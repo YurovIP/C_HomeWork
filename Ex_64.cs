87dd35bcfa42558c18e5221f0d3fbcef53139c49
Yurov I.P <yurov.ip@gmail.com>
2023-05-28 22:24:47 +0300

    added files

---
+++ Ex_64.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+string GetNaturalsBetweenMN(int m, int n)
+{
+    if (m == n) return Convert.ToString(n);
+    return m + ", " + GetNaturalsBetweenMN(m + 1, n);
+}
+
+System.Console.WriteLine("Значение M= ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Значение N= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Натуральные числа в заданном промежутке (от M до N): " + GetNaturalsBetweenMN(m, n));
